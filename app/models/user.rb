387cc7d263e4fda618628acc1aacd792fca6da10
aracelicaes <aracelicaes@gmail.com>
2020-09-22 16:51:27 -0500

    Add User Associations and Methods Add friendships association in User Add inverse_friendships association in User Add friends method in User Add #pending_friends in User Add #friend_requests in User Add #confirm_friend in User Add #friend?(*) in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,37 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  # Confirmed Friendships
+  def friends
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    friends_array.concat(inverse_friendships.map { |friendship| friendship.user if friendship.confirmed })
+    friends_array.compact
+  end
+
+  # Users who have to confirm friend request
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
+
+  # To Confirm a Friend (User)
+  def confirm_friend(user)
+    confirmed_friend = inverse_friendships.find { |friendship| friendship.user == user }
+    confirmed_friend.confirmed = true
+    confirmed_friend.save
+  end
+
+  # To Confirm if it's an existing Friend
+  def friend?(user)
+    friends.include?(user)
+  end
+
+
 end
