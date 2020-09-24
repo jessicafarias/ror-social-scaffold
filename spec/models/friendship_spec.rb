eb2e14cbeb35aab25897dc7033686f73b9b90373
aracelicaes <aracelicaes@gmail.com>
2020-09-24 17:20:40 -0500

    Added a couple more tests to Post Added Validations tests to post

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Friendship, type: :model do
   context '#Associations' do
     it 'Friendship has a foreign_key from user id' do
       association = Friendship.reflect_on_association(:user)
-      expect(association.foreign_key).to eq("user_id")
+      expect(association.foreign_key).to eq('user_id')
     end
 
     it 'Friendship belongs to user' do
@@ -19,12 +19,7 @@ RSpec.describe Friendship, type: :model do
 
     it 'Friendship has a foreign_key from friend table' do
       association = Friendship.reflect_on_association(:friend)
-      expect(association.foreign_key).to eq("friend_id")
-    end
-
-    it 'Friendship does not has many to attendances' do
-      association = Friendship.reflect_on_association(:friend)
-      expect(association.foreign_key).to_not eq(:friend_id)
+      expect(association.foreign_key).to eq('friend_id')
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:user) { User.create(name: 'name', email: 'email@email.com', password: '123456') }
+
   context '#Associations' do
     it 'Post does not have association with friendships table' do
       association = Post.reflect_on_association(:friendships)
@@ -13,9 +15,17 @@ RSpec.describe Post, type: :model do
     end
   end
 
-  context "Validations" do
-    it 'Invalid Post due no user_id atributes' do
-      expect(Post.new(content: 'some text', user_id: 1).valid?).to be false
+  context 'Validations' do
+    it 'Valid Post containing all corresponding atributes' do
+      expect(Post.new(user_id: user.id, content: 'some text')).to be_valid
+    end
+
+    it 'Invalid Post due to missing user_id atributes' do
+      expect(Post.new(content: 'some text')).to_not be_valid
+    end
+
+    it 'Invalid Post due to exceeding characters in content' do
+      expect(Post.new(content: 'This is a LONG post...'.rjust(1001))).to_not be_valid
     end
   end
 end
